409ca405b4e03979230a9280c84a48ade9060dac
albertguo2006 <albertguo2006@gmail.com>
2025-03-21 18:24:12 -0400

    added cuda support

--- src/model.py
+++ src/model.py
@@ -4,7 +4,12 @@ import torch.nn.functional as F
 import math
 
 # Set device
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
 
 
 class GraphAttentionNetwork(nn.Module):
