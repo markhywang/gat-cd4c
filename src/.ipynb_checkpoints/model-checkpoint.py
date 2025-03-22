aafeaab568f940f5de07934ff3a71ec95386b8b6
Albert Guo <aguo@themis.ubuntu.dgp.sandbox>
2025-03-22 04:15:04 -0400

    Convergent Small Dataset Train

---
+++ src/.ipynb_checkpoints/model-checkpoint.py
@@ -0,0 +1,211 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+# Set device
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+
+class GraphAttentionNetwork(nn.Module):
+    def __init__(self, in_features: int, out_features: int, num_edge_features: int,
+                 hidden_size: int, num_layers: int, num_attn_heads: int) -> None:
+        super().__init__()
+
+        if num_layers == 1:
+            layers = [GraphAttentionLayer(in_features, out_features, num_edge_features, use_leaky_relu=False)]
+        else:
+            layers = [GraphAttentionLayer(in_features, hidden_size, num_edge_features, num_attn_heads)]
+            for i in range(num_layers - 2):
+                layers.append(GraphAttentionLayer(hidden_size, hidden_size, num_edge_features, num_attn_heads))
+            layers.append(GraphAttentionLayer(hidden_size, out_features, num_edge_features, use_leaky_relu=False))
+
+        self.gat_layers = nn.Sequential(*layers)
+        self.global_attn_pooling = GlobalAttentionPooling(out_features, 1, 256)
+
+    def forward(self, node_features, edge_features, adjacency_matrix) -> torch.Tensor:
+        # Initial node feature shape: [B, N, F_in]
+        input_tuple = (node_features, edge_features, adjacency_matrix)
+
+        # [B, N, F_in] -> [B, N, F_out]
+        updated_node_features, _, _ = self.gat_layers(input_tuple)
+
+        # Perform global attention pooling for final learning process
+        # [B, N, F_out] -> [B, 1]
+        pchembl_scores = self.global_attn_pooling(updated_node_features)
+
+        # Final shape: [B, 1]
+        return pchembl_scores
+
+
+class GlobalAttentionPooling(nn.Module):
+    def __init__(self, in_features: int, out_features: int = 1, hidden_dim: int = 256) -> None:
+        super().__init__()
+
+        self.global_attn = nn.Linear(in_features, 1)
+        self.dropout = nn.Dropout(0.2)
+
+        self.final_projection = nn.Sequential(
+            nn.Linear(in_features, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, out_features)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        The input node features (x) has shape: [B, N, F_out]
+        """
+        attn_logits = self.global_attn(x)  # [B, N, 1]
+        attn_scores = F.softmax(attn_logits, dim=1)  # Normalize across nodes
+
+        # Apply dropout to attention scores
+        attn_scores = self.dropout(attn_scores)
+
+        # [B, 1, N] @ [B, N, F_out] -> [B, 1, F_out]
+        pooled_features = attn_scores.transpose(1, 2) @ x
+
+        # [B, 1, F_out] -> [B, F_out]
+        pooled_features = pooled_features.squeeze(1)
+        pooled_features = self.dropout(pooled_features)
+
+        # [B, F_out] -> [B, 1]
+        return self.final_projection(pooled_features)
+
+
+class GraphAttentionLayer(nn.Module):
+    def __init__(self, in_features: int, out_features: int,
+                 num_edge_features: int, num_attn_heads: int = 1, use_leaky_relu: bool = True) -> None:
+        super().__init__()
+
+        self.projection = nn.Linear(in_features, out_features)
+        self.layer_norm_1 = nn.LayerNorm(in_features)
+
+        self.use_leaky_relu = use_leaky_relu
+        if use_leaky_relu:
+            self.leaky_relu = nn.LeakyReLU(0.2)
+            self.layer_norm_2 = nn.LayerNorm(out_features)
+
+        self.num_attn_heads = num_attn_heads
+        self.head_size = out_features // num_attn_heads
+        self.attn_matrix = nn.Parameter(torch.empty((num_attn_heads, 2 * self.head_size + num_edge_features)))
+        self.attn_leaky_relu = nn.LeakyReLU(0.2)
+
+        # Final MLP layer because apparently that's what every person does in papers
+        self.out_projection = nn.Linear(out_features, out_features)
+
+        self.dropout = nn.Dropout(0.2)
+
+        # Initialize the projection weights and attention matrix using a Xavier uniform distribution.
+        nn.init.xavier_uniform_(self.projection.weight.data, gain=math.sqrt(2))
+        nn.init.xavier_uniform_(self.attn_matrix.data, gain=math.sqrt(2))
+
+        # Add residual projection if in_features doesn't match out_features.
+        if in_features != out_features:
+            self.residual_proj = nn.Linear(in_features, out_features)
+        else:
+            self.residual_proj = nn.Identity()
+
+    def forward(self, x: tuple[torch.Tensor, torch.Tensor, torch.Tensor]) \
+            -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        # Initial node_features shape: [B, N, F_in]
+        node_features, edge_features, adjacency_matrix = [t.to(device) for t in x]
+        batch_size, num_nodes, num_node_features = node_features.shape
+
+        # Save residual for later addition.
+        residual = node_features
+
+        # [B, N, F_in] -> [B, N, F_out]
+        new_node_features = self.projection(self.layer_norm_1(node_features))
+
+        # Split the node_features for every attention head.
+        # [B, N, F_out] -> [B, N, num_heads, F_out // num_heads]
+        new_node_features = new_node_features.view(batch_size, num_nodes, self.num_attn_heads, -1)
+
+        # attn_coeffs shape: [B, N, N, num_heads]
+        attn_coeffs = self._compute_attn_coeffs(new_node_features, edge_features, adjacency_matrix, num_nodes)
+
+        # [B, N, num_heads, F_out // num_attn_heads] -> [B, N, F_out]
+        new_node_features = self._execute_message_passing(new_node_features, attn_coeffs, batch_size, num_nodes)
+
+        # Do final projection and dropout
+        # The shape remains [B, N, F_out]
+        new_node_features = self.out_projection(new_node_features)
+        new_node_features = self.dropout(new_node_features)
+
+        # Apply residual connection.
+        # If dimensions differ, project the residual to the correct dimension.
+        residual = self.residual_proj(residual)
+        new_node_features = new_node_features + residual
+
+        # Optionally apply layer normalization and activation.
+        if self.use_leaky_relu:
+            new_node_features = self.leaky_relu(self.layer_norm_2(new_node_features))
+
+        return new_node_features, edge_features, adjacency_matrix
+
+    def _compute_attn_coeffs(self, node_features: torch.Tensor, edge_features: torch.Tensor,
+                             adjacency_matrix: torch.Tensor, num_nodes: int) -> torch.Tensor:
+        """
+        Input node_features shape: [B, N, num_heads, F_out // num_heads]
+        """
+        # [B, N, num_heads, F_out // num_heads] -> [B, N, N, num_heads, F_out // num_heads]
+        row_node_features = node_features.unsqueeze(2).repeat(1, 1, num_nodes, 1, 1)
+
+        # [B, N, N, num_heads, F_out // num_heads] -> [B, N, N, num_heads, F_out // num_heads]
+        # Although dimensions are the same, however 2nd and 3rd dimension are transposed
+        col_node_features = row_node_features.transpose(1, 2)
+
+        # [B, N, N, F_edge] -> [B, N, N, num_heads, F_edge]
+        unsqueezed_edge_features = edge_features.unsqueeze(3).repeat(1, 1, 1, self.num_attn_heads, 1)
+
+        # [B, N, N, num_heads, 2 * (F_out // num_heads) + F_edge]
+        attn_input = torch.cat((row_node_features, col_node_features, unsqueezed_edge_features), dim=4)
+
+        # [B, N, N, num_heads, 2 * (F_out // num_heads) + F_edge] @ [2 * (F_out // num_heads) + F_edge, num_heads]
+        # --> [B, N, N, num_heads, num_heads]
+        attn_logits = attn_input @ self.attn_matrix.transpose(0, 1)
+
+        # [B, N, N, num_heads, num_heads] -> [B, N, N, num_heads]
+        attn_logits = attn_logits.sum(dim=-1)
+
+        # [B, N, N] -> [B, N, N, 1]
+        reshaped_adjacency_matrix = adjacency_matrix.unsqueeze(-1)
+
+        # Apply attention masking, similar to that of autoregression
+        # The shape is still [B, N, N, num_heads]
+        attn_logits = attn_logits.masked_fill(reshaped_adjacency_matrix == 0, float('-inf'))
+
+        # Use LeakyReLU then normalize all values using softmax
+        # The shape is still [B, N, N, num_heads]
+        attn_logits = self.attn_leaky_relu(attn_logits)
+        attn_coeffs = F.softmax(attn_logits, dim=2)
+        # Any nodes that don't have any connections (i.e. nodes created to pad the input data to the
+        # required size) will have all their attention logits equal to -inf. In this case, softmax will
+        # output NaN, so replace all NaN values with 0.
+        attn_coeffs = attn_coeffs.nan_to_num(0)
+
+        # Andrej Karpathy does this, so I guess it works (not sure why)
+        # The shape is still [B, N, N, num_heads]
+        attn_coeffs = self.dropout(attn_coeffs)
+
+        # Final shape: [B, N, N, num_heads]
+        return attn_coeffs
+
+    def _execute_message_passing(self, node_features: torch.Tensor, attn_coeffs: torch.Tensor,
+                                 batch_size: int, num_nodes: int) -> torch.Tensor:
+        """
+        node_features shape: [B, N, num_heads, F_out // num_heads]
+        attn_coeffs shape:   [B, N,    N     ,     num_heads     ]
+        """
+        # [B, N, num_heads, F_out // num_heads] EINSUM [B, N, N, num_heads]
+        # -> [B, N, num_heads, F_out // num_heads]
+        new_node_features = torch.einsum('bmax, bnma -> bnax', node_features, attn_coeffs)
+
+        # Concatenate output for different attention heads together.
+        # [B, N, num_heads, F_out // num_heads] -> [B, N, F_out]
+        return new_node_features.view(batch_size, num_nodes, -1)
---
+++ src/.ipynb_checkpoints/train-checkpoint.py
@@ -0,0 +1,252 @@
+import argparse
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import math
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+import torch.nn as nn
+
+from model import GraphAttentionNetwork
+from utils.dataset import DrugProteinDataset
+from utils.helper_functions import *
+
+# Set device
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+print(f"Using device: {device}")
+
+def train_model(args: argparse.Namespace) -> None:
+    # TODO - remove hard-coded specifications for the model
+    # Set the same seed every time for deterministic behaviour.
+    set_seeds()
+
+    model = GraphAttentionNetwork(
+        333,
+        1,
+        16,
+        args.hidden_size,
+        args.num_layers,
+        args.num_attn_heads
+    ).to(torch.float64).to(device)
+    train_dataset, validation_dataset, test_dataset = load_data(args.data_path, args.seed, args.frac_train,
+                                                                args.frac_validation, args.frac_test,
+                                                                args.use_small_dataset)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    validation_loader = DataLoader(validation_dataset, batch_size=args.batch_size, shuffle=False)
+
+    loss_func = nn.SmoothL1Loss(beta=args.huber_beta)  # Initialize the Huber loss function.
+    optimizer = optim.AdamW(
+        model.parameters(),
+        lr=args.lr,
+        weight_decay=args.weight_decay
+    )
+    lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer,
+        factor=args.scheduler_factor,
+        patience=args.scheduler_patience
+    )
+
+    best_validation_loss = float('inf')
+    # Initialize a counter that tracks the number of consecutive epochs without an improvement in validation loss.
+    no_validation_loss_improvement = 0
+    metrics_df = pd.DataFrame(columns=['train_loss', 'validation_loss'], index=range(args.max_epochs))
+
+    for epoch in range(args.max_epochs):
+        progress_bar = tqdm(train_loader, desc=f"Epoch {epoch + 1}/{args.max_epochs}", leave=True)
+        avg_train_loss, avg_train_acc = run_training_epoch(progress_bar, optimizer, model, loss_func)
+        avg_validation_loss, avg_validation_acc = get_validation_metrics(validation_loader, model, loss_func)
+
+        # Step the learning rate scheduler based on mean training loss
+        lr_scheduler.step(avg_train_loss)
+
+        # Save the training and validation metrics in a dataframe.
+        metrics_df.at[epoch, 'train_loss'] = avg_train_loss
+        metrics_df.at[epoch, 'validation_loss'] = avg_validation_loss
+        metrics_df.at[epoch, 'train_acc'] = avg_train_acc
+        metrics_df.at[epoch, 'validation_acc'] = avg_validation_acc
+
+        print(f"Epoch {epoch + 1}/{args.max_epochs}: Train Loss = {avg_train_loss:.5f}, "
+              f"Validation Loss = {avg_validation_loss:.5f}, Train Accuracy = {avg_train_acc:.5f}, "
+              f"Validation Accuracy = {avg_validation_acc:.5f}")
+
+        if avg_validation_loss < best_validation_loss:
+            # Update the best validation loss seen so far.
+            best_validation_loss = avg_validation_loss
+            no_validation_loss_improvement = 0
+            # Save the weights of the model which gives the lowest validation loss so far.
+            torch.save(model.state_dict(), '../models/model.pth')
+        else:
+            no_validation_loss_improvement += 1
+            # If the validation hasn't improved for a certain number of epochs, end training.
+            if no_validation_loss_improvement == args.stoppage_epochs:
+                break
+
+    plot_loss_curves(metrics_df)
+
+
+def run_training_epoch(progress_bar: tqdm, optimizer: optim.Optimizer, model: nn.Module,
+                       loss_func: nn.Module) -> tuple[float, float]:
+    # Ensure model is in training mode.
+    model.train()
+
+    cum_training_samples = 0
+    cum_training_loss = 0
+    cum_training_acc_preds = 0
+
+    for batch_data in progress_bar:
+        node_features, edge_features, adjacency_matrix, pchembl_score = [
+            x.to(torch.float64).to(device) for x in batch_data
+        ]
+
+        preds = model(node_features, edge_features, adjacency_matrix).squeeze(-1)
+        loss = loss_func(preds, pchembl_score)
+
+        cum_training_samples += preds.shape[0]
+        cum_training_loss += loss.item() * preds.shape[0]
+        # Threshold of 7.0 is chosen based on the CD4C paper's claim that a pChEMBL score >= 7.0 signifies a
+        # significant drug-protein interaction.
+        cum_training_acc_preds += accuracy_func(preds, pchembl_score, threshold=7.0)
+
+        optimizer.zero_grad()
+        loss.backward()      
+        optimizer.step()
+
+    avg_loss = cum_training_loss / cum_training_samples
+    avg_acc = cum_training_acc_preds / cum_training_samples
+    return avg_loss, avg_acc
+
+
+def get_validation_metrics(validation_loader: DataLoader, model: nn.Module, loss_func: nn.Module) \
+        -> tuple[float, float]:
+    # Ensure model is in evaluation mode.
+    model.eval()
+
+    cum_validation_samples = 0
+    cum_validation_loss = 0
+    cum_validation_acc_preds = 0
+
+    for batch in validation_loader:
+        node_features, edge_features, adjacency_matrix, pchembl_scores = [
+            x.to(torch.float64).to(device) for x in batch
+        ]
+        preds = model(node_features, edge_features, adjacency_matrix).squeeze(-1)
+        loss = loss_func(preds, pchembl_scores).item()
+        # Threshold of 7.0 is chosen based on the CD4C paper's claim that a pChEMBL score >= 7.0 signifies a
+        # significant drug-protein interaction.
+        acc = accuracy_func(preds, pchembl_scores, threshold=7.0)
+
+        cum_validation_samples += preds.shape[0]
+        cum_validation_loss += loss * preds.shape[0]
+        cum_validation_acc_preds += acc
+
+    # TODO - only plot preds vs labels for the final training epoch
+    # plot_preds_vs_labels(preds, pchembl_scores)
+
+    return cum_validation_loss / cum_validation_samples, cum_validation_acc_preds / cum_validation_samples
+
+
+def load_data(data_path: str, seed: int, frac_train: float, frac_validation: float,
+              frac_test: float, use_small_dataset: bool) -> tuple[Dataset, Dataset, Dataset]:
+    assert math.isclose(frac_train + frac_validation + frac_test, 1), \
+        (f"Sum of training ({frac_train}), validation ({frac_validation}), "
+         f"and testing ({frac_test}) splits don't add up to 1")
+
+    dataset_file = 'filtered_cancer_small.csv' if use_small_dataset else 'filtered_cancer_all.csv'
+    data_df = pd.read_csv(f'{data_path}/{dataset_file}')
+    protein_embeddings_df = pd.read_csv(f'{data_path}/protein_embeddings.csv', index_col=0)
+
+    # Create a column that combines the protein ID and whether there was a significant drug-protein
+    # interaction. This column will be used to split the data to ensure that each dataset has an
+    # appropriate balance of proteins and interaction types.
+    data_df['stratify_col'] = data_df['Target_ID'] + '_' + data_df['label'].astype(str)
+
+    # Split part of the data into the training dataset (maintaining an equal split of proteins and interactions types).
+    train_df, remaining_df = train_test_split(data_df,
+                                              test_size=frac_validation + frac_test,
+                                              stratify=data_df['stratify_col'],
+                                              random_state=seed)
+
+    # Split the remaining data to get the validation and test datasets (maintaining an equal split of proteins and
+    # interaction types).
+    validation_df, test_df = train_test_split(remaining_df,
+                                              test_size=frac_test / (frac_validation + frac_test),
+                                              stratify=remaining_df['stratify_col'],
+                                              random_state=seed)
+
+    # Remove the stratify column from all the datasets.
+    train_df = train_df.drop(columns='stratify_col')
+    validation_df = validation_df.drop(columns='stratify_col')
+    test_df = test_df.drop(columns='stratify_col')
+
+    train_dataset = DrugProteinDataset(train_df, protein_embeddings_df)
+    validation_dataset = DrugProteinDataset(validation_df, protein_embeddings_df)
+    test_dataset = DrugProteinDataset(test_df, protein_embeddings_df)
+
+    return train_dataset, validation_dataset, test_dataset
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--use_small_dataset", action="store_true",
+                        help="Whether to use the small dataset instead of the entire dataset")
+    parser.add_argument("--batch_size", type=int, required=False, default=64,
+                        help="Batch size for data loader")
+    parser.add_argument("--stoppage_epochs", type=int, required=False, default=10,
+                        help="Number of consecutive epochs with no improvement to validation "
+                             "loss before stopping training")
+    parser.add_argument("--max_epochs", type=int, required=False, default=128,
+                        help="Maximum number of epochs to run training")
+    parser.add_argument("--seed", type=int, required=False, default=42,
+                        help="The seed used to control any stochastic operations")
+    # Data parameters
+    parser.add_argument("--data_path", type=str, required=False, default='../data',
+                        help="Path to the folder with the data")
+    parser.add_argument("--frac_train", type=float, required=False, default=0.7,
+                        help="Fraction of data to use for training dataset")
+    parser.add_argument("--frac_validation", type=float, required=False, default=0.15,
+                        help="Fraction of data to use for validation dataset")
+    parser.add_argument("--frac_test", type=float, required=False, default=0.15,
+                        help="Fraction of data to use for test dataset")
+    # Loss parameters
+    parser.add_argument("--huber_beta", type=float, required=False, default=1.0,
+                        help="Beta parameter for Huber loss function")
+
+    # Optimizer parameters
+    parser.add_argument("--weight_decay", type=float, required=False, default=1e-4,
+                        help="Weight decay for optimizer")
+    parser.add_argument("--lr", type=float, required=False, default=3e-3,
+                        help="Learning rate")
+    parser.add_argument("--scheduler_patience", type=int, required=False, default=10,
+                        help="Number of epochs before reducing the learning rate")
+    parser.add_argument("--scheduler_factor", type=float, required=False, default=0.5,
+                        help="The factor in which the learning rate scheduler adjusts learning rate")
+
+    # Model parameters
+    parser.add_argument("--hidden_size", type=int, required=False, default=64,
+                        help="The size of embeddings for hidden layers")
+    parser.add_argument("--num_layers", type=int, required=False, default=3,
+                        help="The number of graph attention layers to use")
+    parser.add_argument("--num_attn_heads", type=int, required=False, default=4,
+                        help="The number of attention heads to use for every attention block")
+    parser.add_argument("--dropout", type=float, required=False, default=0.2,
+                        help="Dropout percentage for graph attention layers")
+    parser.add_argument("--leaky_relu_slope", type=float, required=False, default=0.2,
+                        help="The slope for the Leaky ReLU activation function")
+
+    return parser
+
+
+if __name__ == '__main__':
+    arg_parser = get_parser()
+    training_args = arg_parser.parse_args()
+
+    train_model(training_args)
