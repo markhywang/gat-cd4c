8be4c061d981c9baabff61d2473ad10d30137e68
Albert Guo <aguo@themis.ubuntu.dgp.sandbox>
2025-03-27 20:17:13 -0400

    sigmoid uuwuuwuw :33333

--- src/model.py
+++ src/model.py
@@ -32,7 +32,7 @@ class GraphAttentionNetwork(nn.Module):
         pchembl_scores = self.global_attn_pooling(updated_node_features)
 
         # Normalize pChEMBL scores into the range (0, 14) using hyperbolic tangent
-        pchembl_scores = 7 * (torch.tanh(pchembl_scores) + 1)
+        pchembl_scores = 14 * torch.sigmoid(pchembl_scores)
 
         # Final shape: [B, 1]
         return pchembl_scores
